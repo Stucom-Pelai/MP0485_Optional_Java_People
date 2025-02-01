ff0c030311c288305ad25a1ac825cc55af6d0e85
Japoivvb <jose.portugal@stucom.com>
2025-02-01 11:02:25 +0100

    english option buttons in delete all

--- People/src/main/java/controller/ControllerImplementation.java
+++ People/src/main/java/controller/ControllerImplementation.java
@@ -355,7 +355,19 @@ public class ControllerImplementation implements IController, ActionListener {
     }
 
     public void handleDeleteAll() {
-        int answer = JOptionPane.showConfirmDialog(menu, "Are you sure to delete all people registered?", "Delete All - People v1.1.0", 0, 0);
+        Object[] options = {"Yes", "No"};
+        //int answer = JOptionPane.showConfirmDialog(menu, "Are you sure to delete all people registered?", "Delete All - People v1.1.0", 0, 0);
+        int answer = JOptionPane.showOptionDialog(
+        menu,
+        "Are you sure you want to delete all registered people?", 
+        "Delete All - People v1.1.0",
+        JOptionPane.YES_NO_OPTION,
+        JOptionPane.WARNING_MESSAGE,
+        null,
+        options,
+        options[1] // Default selection is "No"
+    );
+
         if (answer == 0) {
             deleteAll();
         }
